848426c32c5e645053df6226bb8e28373dab06a0
Melvin Jordan <melvin11278@gmail.com>
2025-09-29 16:13:28 +0700

    fix image jpg to png

--- components/about-section.tsx
+++ components/about-section.tsx
@@ -57,7 +57,7 @@ export function AboutSection() {
                 <div className="w-48 h-48 mx-auto bg-gradient-to-br from-primary/20 to-primary/5 newspaper-border flex items-center justify-center mb-4">
                   <div className="w-30 h-32 flex justify-center items-center overflow-hidden border-2 border-primary">
                     <img
-                      src="/foto-profil.png"
+                      src="/foto-profils.png"
                       alt="Avatars"
                       width={300}
                       height={300}
